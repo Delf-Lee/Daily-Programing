8fd509ceda64f3f6ea3733e68cbdafd1809c7060
Delf-Lee <ttjs2008@naver.com>
2018-07-09 23:54:17 +0900

    Upload DP009

---
+++ src/main/java/DP009/Interval.java
@@ -0,0 +1,11 @@
+package DP009;
+
+public class Interval {
+    public int start;
+    public int end;
+
+    public Interval(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+}
