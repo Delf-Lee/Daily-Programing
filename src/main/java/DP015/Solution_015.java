f729186e066ee7a36426de8a9d0fcd3028dbb3a2
Delf-Lee <ttjs2008@naver.com>
2018-08-08 23:56:08 +0900

    Update DP015

---
+++ src/main/java/DP015/Solution_015.java
@@ -0,0 +1,34 @@
+package DP015;
+
+import java.util.Scanner;
+
+public class Solution_015 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int total = scn.nextInt();
+        int n = scn.nextInt();
+        double[] arr = new double[total];
+        for (int i = 0; i < total; i++) {
+            arr[i] = scn.nextInt();
+        }
+
+        double minStdDev = Double.MAX_VALUE;
+        for (int i = 0; i <= arr.length - n; i++) {
+            double sum = 0.0;
+            for (int j = i; j < i + n; j++) {
+                sum += arr[j];
+            }
+            double avg = sum / n;
+            double sumSquare = 0.0;
+            double diff;
+            for (int j = i; j < i + n; j++) {
+                diff = arr[j] - avg;
+                sumSquare += diff * diff;
+            }
+            double stdDev = Math.sqrt(sumSquare / n);
+            minStdDev = stdDev < minStdDev ? stdDev : minStdDev;
+            System.out.println(i);
+        }
+        System.out.println(minStdDev);
+    }
+}
