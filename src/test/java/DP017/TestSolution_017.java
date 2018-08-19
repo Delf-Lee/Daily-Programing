416f57a74b6962daf379ba82b6dc00431a606f4d
Delf-Lee <ttjs2008@naver.com>
2018-08-19 19:47:45 +0900

    Update DP017

---
+++ src/test/java/DP017/TestSolution_017.java
@@ -0,0 +1,22 @@
+package DP017;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class TestSolution_017 {
+
+    private Solution_017 solution = new Solution_017();
+
+    @Test
+
+    public void test1() {
+        boolean canRotate = solution.solution(new int[]{1, 2, 4, 0, 3});
+        canRotate = solution.solution(new int[]{1, 2, 4, 0, 3});
+        assertEquals(true, canRotate);
+        canRotate = solution.solution(new int[]{1, 4, 5, 0, 3, 2});
+        assertEquals(true, canRotate);
+        canRotate = solution.solution(new int[]{1, 2, 2, 0});
+        assertEquals(true, canRotate);
+    }
+}
