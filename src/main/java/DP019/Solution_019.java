ed654a57c5e3cbf56c4e786008b5c0c0d47fe90f
Delf-Lee <ttjs2008@naver.com>
2018-08-21 22:48:32 +0900

    Update DP019

---
+++ src/main/java/DP019/Solution_019.java
@@ -0,0 +1,11 @@
+package DP019;
+
+public class Solution_019 {
+    public int[] solution(int[] arr, int k) {
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = arr[(k + i) % arr.length];
+        }
+        return res;
+    }
+}
---
+++ src/test/java/DP019/SolutionTest_019.java
@@ -0,0 +1,17 @@
+package DP019;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class SolutionTest_019 {
+    private Solution_019 solution = new Solution_019();
+
+    @Test
+    public void test01() {
+
+        assertArrayEquals(new int[]{3, 4, 5, 1, 2}, solution.solution(new int[]{1, 2, 3, 4, 5}, 2));
+
+        assertArrayEquals(new int[]{1, 2, 3, 4, 0}, solution.solution(new int[]{0, 1, 2, 3, 4}, 1));
+    }
+}
