1450f78d0640301e6dac4a9016204cd0034bdad3
Delf-Lee <ttjs2008@naver.com>
2018-09-18 23:47:25 +0900

    Update DP023

---
+++ src/main/java/dailyprogramming/Solution_023.java
@@ -0,0 +1,18 @@
+package dailyprogramming;
+
+import java.util.HashSet;
+import java.util.Set;
+
+// https://programmers.co.kr/learn/courses/30/lessons/42577
+public class Solution_023 {
+    public boolean solution(String[] phoneBook) {
+        Set<String> set = new HashSet<>();
+        for (String s : phoneBook) set.add(s);
+
+        for (String s : phoneBook)
+            for (int i = 0; i < s.length(); i++)
+                if (set.contains(s.substring(0, i)))
+                    return false;
+        return true;
+    }
+}
