30835affbe6478cff449922321c886e2b9b9d993
Delf-Lee <ttjs2008@naver.com>
2018-09-19 23:57:35 +0900

    Update DP024

--- src/main/java/dailyprogramming/Solution_022.java
+++ src/main/java/dailyprogramming/Solution_022.java
@@ -1,6 +1,8 @@
 package dailyprogramming;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 // programmers 알고리즘 문제 풀이
@@ -9,6 +11,7 @@ import java.util.Map;
 // https://programmers.co.kr/learn/courses/30/lessons/42576
 public class Solution_022 {
     public String solution(String[] participant, String[] completion) {
+        List<Integer> list = new ArrayList<>();
         Map<String, Integer> map = new HashMap();
         for (String s : participant)
             map.put(s, map.containsKey(s) ? map.get(s) + 1 : 1);
---
+++ src/main/java/dailyprogramming/Solution_024.java
@@ -0,0 +1,22 @@
+package dailyprogramming;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+// programmers 알고리즘 문제 풀이
+// 위장
+// #해시
+// https://programmers.co.kr/learn/courses/30/lessons/42578
+public class Solution_024 {
+    public int solution(String[][] clothes) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String[] ss : clothes)
+            map.put(ss[1], map.containsKey(ss[1]) ? map.get(ss[1]) + 1 : 1);
+
+        Iterator<String> it = map.keySet().iterator();
+        int result = 1;
+        while (it.hasNext()) result *= map.get(it.next()) + 1;
+        return result - 1;
+    }
+}
