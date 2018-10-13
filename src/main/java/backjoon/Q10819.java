25dbbf7bb7082a332504522d0d66e518b20b37a7
Delf-Lee <ttjs2008@naver.com>
2018-10-13 23:12:50 +0900

    Update backjoon Q10819

---
+++ src/main/java/backjoon/Q10819.java
@@ -0,0 +1,60 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Q10819 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.readLine();
+        int[] arr = Arrays.asList(br.readLine().split(" ")).stream().mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(arr);
+        int ans = 0;
+        do {
+            int temp = calculate(arr);
+            ans = Math.max(ans, temp);
+        } while (next_permutation(arr));
+
+        System.out.println(ans);
+    }
+
+    public static int calculate(int a[]) {
+        int sum = 0;
+        for (int i = 1; i < a.length; i++) {
+            sum += Math.abs(a[i] - a[i - 1]);
+        }
+        return sum;
+    }
+
+    public static boolean next_permutation(int[] a) {
+        int i = a.length - 1;
+        while (i > 0 && a[i - 1] >= a[i]) {
+            i -= 1;
+        }
+
+        if (i <= 0) {
+            return false;
+        }
+
+        int j = a.length - 1;
+        while (a[j] <= a[i - 1]) {
+            j -= 1;
+        }
+
+        int temp = a[i - 1];
+        a[i - 1] = a[j];
+        a[j] = temp;
+
+        j = a.length - 1;
+        while (i < j) {
+            temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+            i += 1;
+            j -= 1;
+        }
+        return true;
+    }
+}
