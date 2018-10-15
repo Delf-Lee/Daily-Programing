fdf31078e52a7d8c6d278118e8fc4454429880ab
Delf-Lee <ttjs2008@naver.com>
2018-10-15 22:43:27 +0900

    Update backjoon Q12827

---
+++ src/main/java/backjoon/Q12827.java
@@ -0,0 +1,27 @@
+package backjoon;
+
+import java.util.Scanner;
+// https://www.acmicpc.net/problem/12871
+// 무한문자열
+public class Q12827 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char[] s = sc.nextLine().toCharArray();
+        char[] t = sc.nextLine().toCharArray();
+        int res = 0;
+        int i = 0, j = 0;
+        while (true) {
+            if(i >= s.length) i = 0;
+            if(j >= t.length) j = 0;
+            if (s[i++] == t[j++]) {
+                if (i == s.length && j == t.length) {
+                    res = 1;
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+        System.out.println(res);
+    }
+}
