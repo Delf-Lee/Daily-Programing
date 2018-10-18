efcc9f2dec0de93c5e46d03dd5006a6833a92f8a
Delf-Lee <ttjs2008@naver.com>
2018-10-18 23:46:51 +0900

    Update backjoon Q1697

---
+++ src/main/java/backjoon/Q1697.java
@@ -0,0 +1,41 @@
+package _workspace;
+
+import java.util.*;
+// https://www.acmicpc.net/problem/1697
+public class Main {
+    static final int MAX = 1000000;
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        boolean[] chk = new boolean[MAX];
+        int[] d = new int[MAX];
+        chk[n] = true;
+        d[n] = 0;
+        Queue<Integer> q = new LinkedList<>();
+        q.add(n);
+        while (!q.isEmpty()) {
+            int x = q.remove();
+            if (x - 1 >= 0 && !chk[x - 1]) {
+                q.add(x - 1);
+                chk[x - 1] = true;
+                d[x - 1] = d[x] + 1;
+            }
+            if (x + 1 < MAX && !chk[x + 1]) {
+                q.add(x + 1);
+                chk[x + 1] = true;
+                d[x + 1] = d[x] + 1;
+            }
+            if (x * 2 < MAX && !chk[x * 2]) {
+                q.add(x * 2);
+                chk[x * 2] = true;
+                d[x * 2] = d[x] + 1;
+
+            }
+        }
+        System.out.println(d[k]);
+    }
+}
+
