92c23f261c36d161525a80a0817c81e5305c4d21
Delf-Lee <ttjs2008@naver.com>
2018-10-22 02:35:46 +0900

    Edit backjoon Q12865

--- src/main/java/backjoon/Q12865.java
+++ src/main/java/backjoon/Q12865.java
@@ -1,34 +1,32 @@
 package backjoon;
 
-import java.util.Arrays;
-import java.util.Scanner;
 
 // https://www.acmicpc.net/problem/12865
 // 평범한 배낭
+
+import java.util.*;
+
 public class Q12865 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int k = sc.nextInt();
-        int[][] dp = new int[n + 1][k + 1];
-        int w[] = new int[n];
-        int v[] = new int[n];
-        for (int i = 0; i < n; i++) {
+        int[][] d = new int[n + 1][k + 1];
+        int w[] = new int[n + 1];
+        int v[] = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
             w[i] = sc.nextInt();
             v[i] = sc.nextInt();
         }
-
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < k; j++) {
-                if (w[i + 1] > j) {
-                    dp[i][j] = dp[i - 1][j];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= k; j++) {
+                if (w[i] > j) {
+                    d[i][j] = d[i - 1][j];
                 } else {
-                    dp[i][j] = Math.max(v[i] + dp[i - 1][j - w[i]], dp[i - 1][j]);
+                    d[i][j] = Math.max(v[i] + d[i - 1][j - w[i]], d[i - 1][j]);
                 }
-
             }
         }
-        System.out.println(dp[n - 1][k - 1]);
+        System.out.println(d[n][k]);
     }
 }
-
