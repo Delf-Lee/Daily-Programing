0ba2d20c3ed0b6129bc28f062c0ffc1b81590d6c
Delf-Lee <ttjs2008@naver.com>
2018-10-22 23:43:04 +0900

    Update backjoon Q12866

---
+++ src/main/java/backjoon/Q12866.java
@@ -0,0 +1,33 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+// https://www.acmicpc.net/problem/12866
+// 피노키오
+public class Q12866 {
+    public static void main(String[] args) throws IOException {
+        long div = 1000000007;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.readLine();
+        String s = br.readLine();
+        Map<Character, Long> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            map.put(c, !map.containsKey(c) ? 1L : map.get(c) + 1);
+        }
+
+        if (map.size() < 4) {
+            System.out.println(0);
+            return;
+        }
+
+        long res = 1;
+        for (Character i : map.keySet()) {
+            res = (res * map.get(i)) % div;
+        }
+        System.out.println(res);
+    }
+}
