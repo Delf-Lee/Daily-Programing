abfab35b38cd9d65397e34422f4201c3b9ba7aa7
Delf-Lee <ttjs2008@naver.com>
2018-10-27 19:13:43 +0900

    Update 2018 Winter coding pre-test question 1

---
+++ src/main/java/dailyprogramming/WinterCoding001.java
@@ -0,0 +1,35 @@
+package dailyprogramming;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class WinterCoding001 {
+    public int solution(String skill, String[] skill_trees) {
+        int answer = 0;
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i < skill.length(); i++) {
+            set.add(skill.charAt(i));
+        }
+        for (String s : skill_trees) {
+            boolean flag = true;
+            int index = 0;
+            for (int i = 0; i < s.length(); i++) {
+                if (set.contains(s.charAt(i))) {
+                    if (skill.charAt(index) == s.charAt(i)) {
+                        index++;
+                    } else {
+                        flag = false;
+                    }
+                }
+            }
+            if(flag) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        WinterCoding001 main = new WinterCoding001();
+        System.out.println(main.solution("CBD", new String[]{"BACDE", "CBADF", "AECB", "BDA"}));
+    }
+}
