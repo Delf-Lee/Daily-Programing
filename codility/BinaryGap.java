db5f4cfc1e59d5637ef33e5549217d7694201413
Delf-Lee <ttjs2008@naver.com>
2019-02-03 20:41:56 +0900

    Update codility solution - BinaryGap

---
+++ codility/BinaryGap.java
@@ -0,0 +1,33 @@
+package codility;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author delf
+ */
+public class BinaryGap {
+    static Set<Integer> set = new HashSet<>();
+
+    static {
+        for (int i = 0; i < 32; i++) set.add((int) Math.pow(2, i));
+    }
+
+    public int solution(int N) {
+        if (set.contains(N)) return 0;
+
+        int max = 0, res = 0;
+        boolean flag = false;
+        while (N > 0) {
+            if (N % 2 == 0) {
+                if (flag) max++;
+            } else {
+                if (!flag) flag = true;
+                res = Math.max(max, res);
+                max = 0;
+            }
+            N /= 2;
+        }
+        return res;
+    }
+}
