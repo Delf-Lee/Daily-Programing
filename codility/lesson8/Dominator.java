9a3aba31f9d58ceeea3bea353d78e4ad441c5f06
Delf-Lee <ttjs2008@naver.com>
2019-03-21 19:03:53 +0900

    Improve codility solution - Dominator

--- codility/lesson8/Dominator.java
+++ codility/lesson8/Dominator.java
@@ -1,34 +1,39 @@
 package codility.lesson8;
 
-import java.util.Stack;
+import java.util.*;
 
 /**
  * @author delf
  */
-// https://app.codility.com/demo/results/trainingHHRTDB-BCB/
 public class Dominator {
-
+    // https://app.codility.com/demo/results/trainingFJGPHF-2U6/
     public int solution(int[] A) {
-        Stack<Integer> stack = new Stack<>();
-        stack.add(A[0]);
-        int res = 0;
-        for (int i = 1; i < A.length; i++) {
-            if (!stack.isEmpty() && A[i] != stack.peek()) {
-                stack.pop();
-            } else {
-                res = i;
-                stack.push(A[i]);
+        int maxCount = -1;
+        int result = -1;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < A.length; i++) {
+            int count = (map.get(A[i]) == null) ? 1 : map.get(A[i]) + 1;
+            map.put(A[i], count);
+
+            if (count == maxCount) {
+                result = -1;
+            }
+            if (count > maxCount) {
+                maxCount = count;
+                result = i;
             }
         }
-        if (stack.isEmpty()) return -1;
-        return res;
+
+        return maxCount > A.length / 2 ? result : -1;
     }
 
 
     public static void main(String[] args) {
-        // System.out.println(new Solution3().solution(new int[]{3, 4, 3, 2, 3, -1, 3, 3}));
-        // System.out.println(new Solution3().solution(new int[]{1}));
+        System.out.println(new Dominator().solution(new int[]{3, 4, 3, 2, 3, -1, 3, 3}));
+        System.out.println(new Dominator().solution(new int[]{2}));
         System.out.println(new Dominator().solution(new int[]{2, 1, 1, 3, 4}));
+        System.out.println(new Dominator().solution(new int[]{2147483647}));
     }
 }
 
