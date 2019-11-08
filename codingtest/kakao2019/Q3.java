db82d32bddf8843846ea97d4b9285c0880c0ea95
Delf-Lee <ttjs2008@naver.com>
2019-11-09 05:59:07 +0900

    Update kakao blind test 2019 Q3

--- codingtest/kakao2019/Q3.java
+++ codingtest/kakao2019/Q3.java
@@ -7,10 +7,8 @@ import java.util.*;
  */
 public class Q3 {
 	public static void main(String[] args) {
-		String word = "???for";
-		System.out.println(word.lastIndexOf('?'));
-		System.out.println(word.substring(word.lastIndexOf('?') + 1));
-
+		System.out.println(Arrays.toString(new Q3().solution(new String[]{"frodo", "front", "frost", "frozen", "frame", "kakao"}, new String[]{"fro??", "????o", "fr???", "fro???", "pro?"})));
+		System.out.println("word".startsWith(""));
 	}
 
 	public int[] solution(String[] words, String[] queries) {
@@ -18,38 +16,50 @@ public class Q3 {
 		Map<Integer, Set<String>> matcher = createMatcher(words);
 		for (String query : queries) {
 			if (!matcher.containsKey(query.length())) {
+				answer.add(0);
 				continue;
 			}
+			answer.add(countWord(query, matcher.get(query.length())));
 		}
 
 		return answer.stream().mapToInt(i -> i).toArray();
 	}
 
-	private int findWords(String query, Set<String> dictionary) {
+	private int countWord(String query, Set<String> dictionary) {
 		int result = 0;
+		String modified = getWordStartWithAlphabet(query);
 		for (String word : dictionary) {
-			getCore(word);
+			result += word.startsWith(getCore(modified)) ? 1 : 0;
 		}
 
 		return result;
 	}
 
 	private String getCore(String query) {
-		if (query.charAt(0) != '?') {
-			return query.substring(0, query.indexOf('?'));
-		}
-		return query.substring(query.lastIndexOf('?') + 1);
+		return query.substring(0, query.indexOf("?"));
 	}
 
 	private Map<Integer, Set<String>> createMatcher(String[] words) {
 		Map<Integer, Set<String>> matcher = new HashMap<>();
 		for (String word : words) {
+			String modified = (new StringBuilder(word)).reverse().toString();
+
 			if (!matcher.containsKey(word.length())) {
-				matcher.put(word.length(), new HashSet<>(Collections.singletonList(word)));
+				matcher.put(word.length(), new HashSet<>(Arrays.asList(word, modified)));
 			}
+
+			matcher.get(word.length()).add(modified);
 			matcher.get(word.length()).add(word);
 		}
+
 		return matcher;
+	}
+
+	private String getWordStartWithAlphabet(String word) {
+		if (word.startsWith("?")) {
+			return (new StringBuilder(word)).reverse().toString();
+		}
+		return word;
 	}
 
 }
