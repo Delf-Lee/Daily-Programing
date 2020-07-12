9d31bd5551381db758d160b06c4f2b69e5cb2db4
Delf-Lee <ttjs2008@naver.com>
2020-07-12 23:56:32 +0900

    Update programmers algorithm practice 42626

---
+++ dailyprogramming/programmers/Q42626.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Solution42626 {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        for (int n : scoville) {
+            priorityQueue.add(n);
+        }
+
+        while (!priorityQueue.isEmpty() && priorityQueue.peek() < K) {
+            int lowest = priorityQueue.poll();
+            if (priorityQueue.isEmpty()) {
+                return -1;
+            }
+            int merge = lowest + (priorityQueue.poll() * 2);
+            if(merge == 0) {
+                return -1;
+            }
+            priorityQueue.add(merge);
+            answer++;
+        }
+
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(new Solution42626().solution(new int[]{0,0,0,0}, 7));
+    }
+}
