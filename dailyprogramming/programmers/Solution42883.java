c4422abd4e59a55d65e2456472689092878acf99
Delf-Lee <ttjs2008@naver.com>
2020-09-28 23:39:35 +0900

    Update programmers 42883

---
+++ dailyprogramming/programmers/Solution42883.java
@@ -0,0 +1,21 @@
+public class Solution42883 {
+    public static void main(String[] args) {
+        System.out.println(new Solution42883().solution("1924", 2));
+    }
+    String solution(String number, int k) {
+        StringBuilder answer = new StringBuilder();
+        //필요한 글자수만큼 반복하기
+        for (int j = 0, index = -1; j < number.length() - k; j++) {
+            char max = 0;
+            //앞에서 남겨야 되는 숫자 중에 제일 큰수 체크
+            for (int i = index + 1; i <= k + j; i++) {
+                if (max < number.charAt(i)) {
+                    index = i;
+                    max = number.charAt(i);
+                }            }
+            //제일 큰수를 정답에 추가
+            answer.append(max);
+        }
+        return answer.toString();
+    }
+}
